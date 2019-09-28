80bbd254ef8a02fc6217db98c706c14f9060c063
Jacob5122 <ffhelios2@gmail.com>
2019-09-28 21:29:50 +0200

    Create turtle_4-5.py

---
+++ School/Python_1/Seminar_1/turtle_4-5.py
@@ -0,0 +1,17 @@
+import turtle
+
+def odmocnina(n):
+	turtle.forward(100)
+	turtle.left(90)
+	turtle.forward(100)
+	for i in range (n-1):
+		rot = turtle.towards(0, 0)
+		dist = turtle.distance(0, 0)
+		turtle.setheading(rot)
+		turtle.forward(dist)
+		turtle.backward(dist)
+		turtle.right(90)
+		turtle.forward(100)
+	print("%.2f" % (dist/100))
+
+odmocnina(6)
