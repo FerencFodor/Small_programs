1f51ef84338109737b06c67739c43234e5cad7b8
Jacob5122 <ffhelios2@gmail.com>
2019-09-28 21:39:47 +0200

    Update turtle_4-5.py

--- School/Python_1/Seminar_1/turtle_4-5.py
+++ School/Python_1/Seminar_1/turtle_4-5.py
@@ -1,19 +1,19 @@
-import turtle
+﻿import turtle
 
-#Calculates the square root of the number using turtle graphics
+# Vypočíta odmocninu cisla pomocou turtle grafiky
 
 def odmocnina(n):
 	turtle.forward(100)
 	turtle.left(90)
 	turtle.forward(100)
 	for i in range (n-1):
-		rot = turtle.towards(0, 0)
-		dist = turtle.distance(0, 0)
-		turtle.setheading(rot)
-		turtle.forward(dist)
+		rot = turtle.towards(0, 0)	# vypočíta uhol na ktorý sa má natočiť na bod 0,0
+		dist = turtle.distance(0, 0)	# vypočíta vzdialenosť od bodu 0,0
+		turtle.setheading(rot)		# natočí 'koritnačku' podla uhlu daným premennou rot
+		turtle.forward(dist)		# posunie 'koritnačku' podla vzdialenosti daným premonnou dist
 		turtle.backward(dist)
 		turtle.right(90)
 		turtle.forward(100)
-	print("%.2f" % (dist/100))
+	print("%.2f" % (dist/100))		# Kvôli grafickéhu znázorneniu je potrebné výsledok vydeliť 100
 
-odmocnina(6)
+odmocnina(6)					#Volanie funkcie
